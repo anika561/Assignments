51db7bbbd21143bb4efcc71da8c17d6a4e92b247
The Edutarian <anikagupta18.04@gmail.com>
2020-12-11 23:36:16 +0530

    assignment20

---
+++ Assignment 20/Assignment 20.js
@@ -0,0 +1,13 @@
+alert("Hey! Buddy\nWelcome to my site :)");
+alert("Are you a begginer\nor an expert in JAVASCRIPT(J.S)")
+
+var firstname=prompt("Type your first name")
+var lastname=prompt("Type your last name")
+var age=prompt("Type your age") 
+var name =`${firstname} ${lastname}`;
+
+alert(`Your full name is ${name}`);
+alert(`You are ${age} years old`);
+
+console.log(`Your full name is ${name}`);
+console.log(`You are ${age} years old`);
