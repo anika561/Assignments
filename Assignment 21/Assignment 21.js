40fc9b4079b2784f8bb06848568701b72d6e358b
Aniket Gupta <anikagupta18.04@gmail.comgit config --global user.name anika561git config --global user.email anikagupta18.04@gmail.com>
2021-01-19 22:44:22 +0530

    assignment 21

---
+++ Assignment 21/Assignment 21.js
@@ -0,0 +1,15 @@
+alert("Hey! User")
+
+var age= prompt("What is your age?")
+var age= alert( `Your roughly age is  ${age*365}`)
+console.log(`Your roughly age is  ${age*365}`)
+
+var length= prompt("What is length of rectangle?")
+var breadth= prompt("What is breadth of rectangle?")
+var area= alert(`Area of rectangle is  ${ length * breadth}`)
+console.log(`Area of rectangle is  ${ length * breadth}`)
+
+var base= prompt("What is base of triangle?")
+var height= prompt("What is height of triangle?")
+var area= alert(`Area of triangle is  ${1/2 * base * height}`)
+console.log(`Area of triangle is  ${1/2 * base * height}`)
